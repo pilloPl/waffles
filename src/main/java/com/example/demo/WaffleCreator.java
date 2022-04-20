49619a1bd6faf9e4d0da270115904daf10b95579
jpilimon <jakub.pilimon@gmail.com>
2022-04-20 14:16:40 +0200

    spring skeleton

--- src/main/java/com/example/demo/WaffleCreator.java
+++ src/main/java/com/example/demo/WaffleCreator.java
@@ -2,11 +2,15 @@ package com.example.demo;
 
 import org.springframework.beans.factory.NoSuchBeanDefinitionException;
 import org.springframework.beans.factory.NoUniqueBeanDefinitionException;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+
 
 class WaffleCreator {
 
@@ -35,51 +39,6 @@ class WaffleCreator {
 
 }
 
-class BeanDefinition {
-
-    String name;
-    Object dep;
-
-    BeanDefinition(String name, Object dep) {
-        this.name = name;
-        this.dep = dep;
-    }
-}
-
-class BeanFactory {
-
-    static Map<Class, List<BeanDefinition>> providers = new HashMap<>();
-
-    static void put(String name, Object object) {
-        put(name, object, object.getClass());
-    }
-
-    public static void put(String name, Object object, Class cls) {
-        List<BeanDefinition> deps = providers.getOrDefault(cls, new ArrayList<>());
-        deps.add(new BeanDefinition(name, object));
-        providers.put(cls, deps);
-    }
-
-    static <T> T get(Class<T> cls) {
-        List<BeanDefinition> deps = providers.get(cls);
-        if (deps.size() == 1) {
-            return cls.cast(deps.get(0));
-        }
-        throw new NoUniqueBeanDefinitionException(cls);
-    }
-
-    static <T> T get(Class<T> cls, String name) {
-        return providers.getOrDefault(cls, new ArrayList<>())
-                .stream()
-                .filter(dep -> dep.name.equals(name))
-                .map(dep -> cls.cast(dep.dep))
-                .findFirst()
-                .orElseThrow(() -> new NoSuchBeanDefinitionException(cls));
-
-    }
-
-
-}
 
 
 class TooSweetWaffle extends RuntimeException {
