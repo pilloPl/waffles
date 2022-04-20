1be99dfd2328bf5b90ea7a7b1f38684d7c3e400c
jpilimon <jakub.pilimon@gmail.com>
2022-04-20 14:34:07 +0200

    testing with mock bean

--- src/test/java/com/example/demo/WaffleCreatorTest.java
+++ src/test/java/com/example/demo/WaffleCreatorTest.java
@@ -1,29 +1,34 @@
 package com.example.demo;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+@SpringBootTest
 class WaffleCreatorTest {
 
+    @Autowired
     WaffleCreator waffleCreator;
 
-    @BeforeEach
-    void setup() {
-        MacronutrientsProvider fit = Mockito.mock(MacronutrientsProvider.class);
-        MacronutrientsProvider fat = Mockito.mock(MacronutrientsProvider.class);
-        Mockito.when(fit.fetch()).thenReturn(new Macronutrients(10, 40, 10));
-        Mockito.when(fat.fetch()).thenReturn(new Macronutrients(100, 40, 10));
-        waffleCreator = new WaffleCreator(fit, fat);
-    }
+    @MockBean
+    @Autowired
+    @Qualifier("fit")
+    MacronutrientsProvider fit;
+
+    @MockBean
+    @Autowired
+    @Qualifier("fat")
+    MacronutrientsProvider fat;
 
     @Test
     void canCreateSweetWaffle() {
         //when
+        Mockito.when(fat.fetch()).thenReturn(new Macronutrients(100, 10,10));
         Waffle withSugar = waffleCreator.prepare("FIT DIAMOND", Waffle.Type.SUPER_SWEET);
 
         //then
@@ -33,8 +38,7 @@ class WaffleCreatorTest {
     @Test
     void canCreateFitWaffle() {
         //when
-        //MacronutrientsProvider mock = Mockito.mock(MacronutrientsProvider.class);
-        //Mockito.when(mock.fetch()).thenReturn(new Macronutrients(10, 40, 10));
+        Mockito.when(fit.fetch()).thenReturn(new Macronutrients(10, 50,10));
         Waffle fit = waffleCreator.prepare("FIT DIAMOND", Waffle.Type.LOW_SUGAR);
 
         //then
